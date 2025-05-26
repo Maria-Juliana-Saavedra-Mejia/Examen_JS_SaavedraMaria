510836b7a5625022358e4d7830675d4c87d155df
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-05-26 09:40:54 -0500

    feat :sparkles: api advance

--- script/recetas.js
+++ script/recetas.js
@@ -1,22 +1,23 @@
 console.log("holasssss");
-const urlapi= "https://www.themealdb.com/api/json/v1/1/random.php";
-const busquedas=  document.getElementById('busqueda');
+async function mostrarmealss() {
+    console.log("holaaaaaa")
+    let meal= document.getElementById("comidas")
+    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`);
+    console.log(response.data.meals)
+    const data = response.data;
+    for (let i = 0; i < data.meals.length; i++) {
+        let meals = data.meals[i];
+        let nombre = meals.strMeal;
+        let img = meals.strMealThumb;
+        let category= meals.strCategory;
 
-async function data(urlapi){
-    const response= await fetch (urlapi);
-    const data = await response.json();
-    return data;
-}
-console.log(data(urlapi))
-async function Comidas(){
-    console.log(data(urlapi))
-    console.log(data(urlapi).meals)
-    
-    for (let i = 0; i < 20; i++) {
-        let comidas = data.comidas[i];
-        let imagen = comidas.nombre;
-        let Categoria = comidas.imagen;
-        let TipoComida = comidas.imagen;
-        console.log(data(comidas))
+        meal.innerHTML += `
+        <div class="cosita" idmeals="${meals.id}">
+            <img class="images" src="${img}"/>
+            <h1 class="circuitNames">${nombre}</h1>
+            <h1 class="circuitNames">${category}</h1>
+        </div>
+`
     }
 }
+mostrarmealss()
