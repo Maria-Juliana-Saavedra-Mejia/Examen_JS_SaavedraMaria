e210caf606f25a9df5d5e454e1005684fe1bb51b
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-05-26 08:07:52 -0500

    feat :sparkles: new updates

---
+++ script/index.js
@@ -0,0 +1,21 @@
+console.log("holiiiiii")
+class BarraSuperior extends HTMLElement{
+    constructor(){
+        let estilos=`<style>
+        .logo{
+            position: absolute;
+            width: 10vw;
+            top: 1vw;
+            left: 3vw;
+        }
+        </style>`;
+      super();
+      this.attachShadow({mode:'open'});
+
+
+        this.shadowRoot.innerHTML =`
+        ${estilos}
+       <img class="logo" src="./img/gorroCocina.png">`;  
+    }
+}
+customElements.define('user-card',BarraSuperior);
